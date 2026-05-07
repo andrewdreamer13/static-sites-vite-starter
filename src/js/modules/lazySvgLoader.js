42d2e76c01f11a4832f1817bc34f3a44c757aea6
Andrew Dreamer <andrewdreamer666@gmail.com>
2026-05-07 12:05:01 +0300

    Improved the function initLazySvg. Add fetch for heavy svg

--- src/js/modules/lazySvgLoader.js
+++ src/js/modules/lazySvgLoader.js
@@ -1,5 +1,6 @@
 export const initLazySvg = () => {
   const lazyContainers = document.querySelectorAll("[data-svg-receiver]");
+
   if (lazyContainers.length === 0) return;
 
   const options = {
@@ -7,30 +8,65 @@ export const initLazySvg = () => {
     threshold: 0.01,
   };
 
+  const insertSvg = (container, content) => {
+    container.innerHTML = "";
+    container.appendChild(content);
+  };
+
+  const loadFromTemplate = (container, template, id) => {
+    const clone = template.content.cloneNode(true);
+
+    insertSvg(container, clone);
+
+    console.log(`SVG ${id} loaded from template`);
+  };
+
+  const loadFromFetch = async (container, src, id) => {
+    const response = await fetch(src);
+
+    if (!response.ok) {
+      throw new Error(`Network error: ${response.status}`);
+    }
+
+    const svgText = await response.text();
+
+    container.innerHTML = svgText;
+
+    console.log(`SVG ${id} loaded with fetch`);
+  };
+
+  const handleIntersection = async (entry, observer) => {
+    if (!entry.isIntersecting) return;
+
+    const container = entry.target;
+
+    const id = container.dataset.svgId;
+    const src = container.dataset.svgSrc;
+
+    const template = document.getElementById(id);
+
+    try {
+      if (template?.content) {
+        loadFromTemplate(container, template, id);
+      } else if (src) {
+        await loadFromFetch(container, src, id);
+      }
+
+      container.classList.add("_is-loaded");
+
+      observer.unobserve(container);
+    } catch (error) {
+      console.error(`Lazy loading error ${id}:`, error);
+    }
+  };
+
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const container = entry.target;
-        const templateId = container.dataset.svgId;
-        const template = document.getElementById(templateId);
-
-        if (template) {
-          const svgContent = template.content.cloneNode(true);
-
-          container.innerHTML = "";
-          container.appendChild(svgContent);
-
-          container.classList.add("_is-loaded");
-
-          observer.unobserve(container);
-
-          console.log(`SVG ${templateId} successfully inlined.`);
-        } else {
-          console.warn(`Template with id "${templateId}" not found!`);
-        }
-      }
+      handleIntersection(entry, observer);
     });
   }, options);
 
-  lazyContainers.forEach((container) => observer.observe(container));
+  lazyContainers.forEach((container) => {
+    observer.observe(container);
+  });
 };
