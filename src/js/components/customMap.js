7327ada891be096629d69431ae5a07194b087fdf
Andrew Dreamer <andrewdreamer666@gmail.com>
2026-07-18 15:10:19 +0300

    feat: add mapbox custom map implementation and style structure

--- src/js/components/customMap.js
+++ src/js/components/customMap.js
@@ -3,7 +3,6 @@ import mapboxgl from "mapbox-gl";
 export const initCustomMap = (container) => {
   const { mapLat, mapLng, mapId } = container.dataset;
 
-  // Твой публичный токен Mapbox
   mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
   const map = new mapboxgl.Map({
@@ -11,13 +10,22 @@ export const initCustomMap = (container) => {
     style: "mapbox://styles/andrew-dreamer/cmrp3l942005y01qkavid5m8b",
     center: [parseFloat(mapLng), parseFloat(mapLat)],
     zoom: 15,
+    
   });
+
+  map.addControl(new mapboxgl.FullscreenControl());
+  map.addControl(new mapboxgl.NavigationControl());
+  map.addControl(
+    new mapboxgl.ScaleControl({
+      maxWidth: 80,
+      unit: "metric", 
+    }),
+  );
 
   new mapboxgl.Marker()
     .setLngLat([parseFloat(mapLng), parseFloat(mapLat)])
     .addTo(map);
 
   container.classList.add("_is-loaded");
-  console.log("Mapbox custom map loaded");
   console.log("Custom map loaded");
 };
